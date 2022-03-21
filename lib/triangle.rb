7d5a0019be6bbfe8c54bfa10bf8e13a45f66dc13
Ibaihima <ibaihima@hotmail.com>
2022-03-21 13:06:35 -0400

    commit tree a823d9899abbd06ca2f76e75aef7224d2fb48142

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
   # write code here
+
+end
+class Triangle
+  # triangle code
+  attr_accessor :a, :b, :c
+  def initialize (a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end 
+  def kind
+    if @a = 0 || @b = 0 || @c = 0
+      raise TriangleError
+    elsif @a < 0 || @b < 0 || @c < 0
+      raise TriangleError
+    elsif ( @a != @b ) && ( @c != @b)  && ( @a != @c)
+      :scalene
+    elsif (@a + @b + @c) % 3 == 0 
+      :equilateral
+    elsif (@a + @b) % 2 == 0 || (@a + @c) % 2 == 0 || (@b + @c) % 2 == 0
+      :isosceles
+    end
+  end  
+
+  class TriangleError < StandardError
+    # triangle error code
+    def message 
+      "error"
+    end
+  end
 end
